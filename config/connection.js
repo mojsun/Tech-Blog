57d45b8775b4f827303c1004f676d9aa77b46880
mojsun <m.khorashahi7@gmail.com>
2023-03-06 23:18:02 -0500

    fixing errors

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,13 @@ require("dotenv").config(); // Import sensitive data from .env
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        // This is for Local
-        host: "localhost",
-        dialect: "mysql",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+  : new Sequelize("blog_db", "root", "@Mojde1373", {
+      // This is for Local
+      host: "localhost",
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
